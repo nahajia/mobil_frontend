d88a786b2b67aa62c12da48ec936f4d81897e661
Aniko <nahajia@gmail.com>
2023-12-08 16:06:19 +0100

    aaa

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Proba from './Proba';
+import Mogyoro from "./Mogyoro"
+import Repa from './Repa';
+import Retek from "./Retek"
+import Kepfeltoltes from "./Kepfeltoltes"
+import Felvitel from "./Felvitel"
+import Lenyiloteszt from "./Lenyiloteszt"
+import Lenyilo from "./Lenyilo"
 
 function HomeScreen({ navigation }) {
   return (
@@ -9,6 +18,10 @@ function HomeScreen({ navigation }) {
       <Button
         onPress={() => navigation.navigate('Notifications')}
         title="Go to notifications"
+      />
+       <Button
+        onPress={() => navigation.navigate('Proba')}
+        title="Próba screen meghívása"
       />
     </View>
   );
@@ -21,16 +34,36 @@ function NotificationsScreen({ navigation }) {
     </View>
   );
 }
+function Root({ navigation }) {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Screen name="Home" component={HomeScreen} />
+    <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    <Drawer.Screen name="Mogyoro" component={Mogyoro} />
+    <Drawer.Screen name="Kepfeltotles" component={Kepfeltoltes} />  
+    <Drawer.Screen name="Felvitel" component={Felvitel} />     
+    <Drawer.Screen name="Lenyiloteszt" component={Lenyiloteszt} /> 
+    <Drawer.Screen name="Lenyilo" component={Lenyilo} /> 
+    </Drawer.Navigator>
+
+  );
+}
+
+
+
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator >
+        <Stack.Screen name="Root" component={Root} options={{headerShown:false}} />
+        <Stack.Screen name="Proba" component={Proba}  />  
+        <Stack.Screen name="Repa" component={Repa}  />       
+        <Stack.Screen name="Retek" component={Retek}  />       
+      </Stack.Navigator>     
     </NavigationContainer>
   );
 }
---
+++ Kepfeltoltes.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Button, Image, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
+  const SERVER_URL = 'http://192.168.0.197:3000';
+
+  const createFormData = (photo, body = {}) => {
+    const data = new FormData();
+
+    data.append('photo', {
+      name: 'photo.jpg',
+      type: 'image/jpg',
+      uri: Platform.OS === 'ios' ? photo.uri.replace('file://', '') : photo.uri,
+    });
+
+    Object.keys(body).forEach((key) => {
+      data.append(key, body[key]);
+    });
+
+    return data;
+  };
+
+  const handleUploadPhoto = async () => {
+    try {
+      if (!image) {
+        console.log('Please select an image first');
+        return;
+      }
+
+      const response = await fetch(`${SERVER_URL}/api/upload`, {
+        method: 'POST',
+        body: createFormData(image, { userId: '123' }),
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network request failed');
+      }
+
+      const data = await response.json();
+      console.log('response', data);
+    } catch (error) {
+      console.log('error', error.message);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Upload Photo" onPress={handleUploadPhoto} />
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />}
+    </View>
+  );
+}
